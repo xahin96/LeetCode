125d17564522df8767f77c07f020809f2f020db0
zahin <xahin96@gmail.com>
2024-05-24 17:07:01 -0400

    3 added

---
+++ 3 Longest Substring Without Repeating Characters.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let left = 0;
+    let right = 0;
+    const charMap = new Map();
+    let maxLen = 0;
+
+    for ( ; right < s.length; right++){
+        if (charMap.has(s[right])){
+            left = Math.max(charMap.get(s[right]) + 1, left);
+        }
+        charMap.set(s[right], right);
+        maxLen = Math.max(maxLen, (right - left) + 1);
+    }
+    return maxLen;
+};
