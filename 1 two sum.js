4b6ff8593c92e74bb935c6775e5e3af1b31f32d2
zahin <xahin96@gmail.com>
2024-05-25 22:51:58 -0400

    codes added

--- 1 two sum.js
+++ 1 two sum.js
@@ -3,17 +3,33 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
+// var twoSum = function(nums, target) {
+//     debugger;
+//     let left = 0;
+//     let right = 1;
+//     for (; right < nums.length ; right++){
+//         if (nums[left] + nums[right] === target){
+//             return [left, right];
+//         } else if (right === nums.length - 1){
+//             left++;
+//             right = left;
+//         }
+//     }
+// };
+// console.log(twoSum([3,2,4], 6));
+
+
+const twoSum2 = (nums, target) =>  {
     debugger;
-    let left = 0;
-    let right = 1;
-    for (; right < nums.length ; right++){
-        if (nums[left] + nums[right] === target){
-            return [left, right];
-        } else if (right === nums.length - 1){
-            left++;
-            right = left;
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (map.has(complement)) {
+            console.log(map);
+            return [map.get(complement), i];
         }
+        map.set(nums[i], i);
     }
+    return -1;
 };
-console.log(twoSum([3,2,4], 6));
+console.log(twoSum2([3,2,4,5,1,7,8], 15));
