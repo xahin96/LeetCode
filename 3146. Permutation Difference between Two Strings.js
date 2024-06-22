5d5ba6dd4749a04f18b80a57f45f022a70b6e9eb
zahin <xahin96@gmail.com>
2024-06-22 00:48:28 -0400

    codes added

---
+++ 3146. Permutation Difference between Two Strings.js
@@ -0,0 +1,11 @@
+var findPermutationDifference = function(s, t) {
+    let hMap = new Map();
+    for(let i = 0 ; i < s.length ; i++){
+        hMap.set(s[i], i);
+    }
+    let diff = 0;
+    for(let i = 0 ; i < t.length ; i++){
+        diff = diff + Math.abs(hMap.get(t[i]) - i)
+    }
+    return diff;
+};
