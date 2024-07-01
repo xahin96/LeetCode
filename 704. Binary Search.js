8f1f0755d5f545ac5ae43aacf770f5610731f90d
zahin <xahin96@gmail.com>
2024-07-01 17:07:38 -0400

    codes added

---
+++ 704. Binary Search.js
@@ -0,0 +1,18 @@
+
+var search = function(nums, target) {
+    let l = 0;
+    let r = nums.length - 1;
+
+    while(l <= r)
+    {
+        mid = l + Math.floor((r - l) / 2);
+        if(nums[mid] < target){
+            l = mid + 1;
+        } else if(nums[mid] > target){
+            r = mid - 1;
+        } else {
+            return mid;
+        }
+    }
+    return -1;
+};
