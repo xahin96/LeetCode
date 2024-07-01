c2fbaf3c2ffcaf6d413a97fee274926a79e38e69
zahin <xahin96@gmail.com>
2024-07-01 17:02:47 -0400

    codes added

---
+++ 704. Binary Search.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.Length - 1;
+        while(l <= r)
+        {
+            int mid = (l + r) / 2;
+            if(nums[mid] < target)
+            {
+                l = mid + 1;
+            }
+            else if(nums[mid] > target)
+            {
+                r = mid - 1;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
