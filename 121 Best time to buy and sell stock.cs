a07a17c44a1c86bd64fe95936d444ad35bfd9964
zahin <xahin96@gmail.com>
2025-03-24 13:39:05 -0400

    121 c#

---
+++ 121 Best time to buy and sell stock.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        if (prices.Length == 1)
+        {
+            return 0;
+        }
+        int maxProfit = 0;
+        int l = 0;
+        int r = 1;
+        for (int i = 0; i < prices.Length - 1; i++)
+        {
+            if (prices[r] - prices[l] > maxProfit)
+            {
+                maxProfit = prices[r] - prices[l];
+                r++;
+            }
+            else if (prices[r] < prices[l])
+            {
+                l=r;
+                r++;
+            }
+            else
+            {
+                r++;
+            }
+        }
+        return maxProfit;
+    }
+}
